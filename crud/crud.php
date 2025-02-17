91def01fee91329d912ed9f87e95ad3bcc70b706
Arnaud <arnaudpf2005@gmail.com>
2025-02-17 11:16:26 +0100

    post method

--- crud/crud.php
+++ crud/crud.php
@@ -1,6 +1,23 @@
 <?php 
 
-require_once 'config/config.php';
+require_once '../config/config.php';
+
+
+function postArticle($name, $slug, $description, $price) {
+    global $conn;
+    $sql = "INSERT INTO article (Name, Slug, Description, Price, Publication_Date, Modification_Date) 
+        VALUES (?, ?, ?, ?, NOW(), NOW())";   
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssd", $name, $slug, $description, $price);
+    if ($stmt->execute()) {
+        echo "good";
+        return $conn->insert_id;
+    } else {
+        echo "failed to create article";
+    }
+    $stmt->close();
+}
 
 function getAllArticles() {
     global $conn;
@@ -8,7 +25,6 @@ function getAllArticles() {
     $sql = "SELECT * FROM article";
     $result = $conn ->query($sql);
     $articles = [];
-
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $articles[] = $row;
@@ -17,9 +33,32 @@ function getAllArticles() {
     return $articles;
 }
 
+function getArticlesByName($name) {
+    global $conn;
+    $sql = "SELECT * FROM article WHERE name LIKE '%$name%'";
+    $result = $conn ->query($sql);
+    $articles = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $articles[] = $row;
+        }
+    }
+    return $articles;
+}
+
+function getArticleById($id) {
+    global $conn;
+    $sql = "SELECT * FROM article WHERE id = $id";
+    $result = $conn ->query($sql);
+    $article = $result->fetch_assoc();
+    return $article;
+}
 
 
 
+
+
+postArticle("Guitare", "instrument", "instrument de musique", 100);
 
 
 ?>
